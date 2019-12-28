36f1a9373ed2f8097ae61bef3c3a7aee18dda525
Christopher Bartling <chris.bartling@gmail.com>
2019-12-28 01:08:26 -0600

    Added annotate gem.

--- nfl-mock-draft-rails/app/models/team.rb
+++ nfl-mock-draft-rails/app/models/team.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id           :uuid             not null, primary key
+#  name         :string(60)       not null
+#  slug         :string(60)       not null
+#  svg_logo_url :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_teams_on_name  (name) UNIQUE
+#  index_teams_on_slug  (slug) UNIQUE
+#
+
 class Team < ApplicationRecord
   self.implicit_order_column = "created_at"
 
--- nfl-mock-draft-rails/test/factories/teams.rb
+++ nfl-mock-draft-rails/test/factories/teams.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id           :uuid             not null, primary key
+#  name         :string(60)       not null
+#  slug         :string(60)       not null
+#  svg_logo_url :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_teams_on_name  (name) UNIQUE
+#  index_teams_on_slug  (slug) UNIQUE
+#
+
 FactoryBot.define do
   factory :team do
     
