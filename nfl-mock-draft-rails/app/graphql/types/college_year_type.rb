eb2033dc1007fb21c23497c1b97ea1350edc8701
Christopher Bartling <chris.bartling@gmail.com>
2020-01-02 19:13:30 -0600

    Adding description to enum type.

--- nfl-mock-draft-rails/app/graphql/types/college_year_type.rb
+++ nfl-mock-draft-rails/app/graphql/types/college_year_type.rb
@@ -1,9 +1,9 @@
 module Types
   class CollegeYearType < Types::BaseEnum
-    value 'FRESHMAN'
-    value 'SOPHOMORE'
-    value 'JUNIOR'
-    value 'SENIOR'
-    value 'GRADUATE'
+    value 'FRESHMAN', description: 'Freshman'
+    value 'SOPHOMORE', description: 'Sophomore'
+    value 'JUNIOR', description: 'Junior'
+    value 'SENIOR', description: 'Senior'
+    value 'GRADUATE', description: 'Graduate'
   end
 end
